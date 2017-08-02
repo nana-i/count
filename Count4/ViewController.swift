ff5f50ebbaa1a8e36a9324c7aaa113758e7554bd
nttr <nttr@BI-23565.local>
2017-08-02 14:27:17 +0900

    カウンターアプリの基本機能を追加

--- Count4/ViewController.swift
+++ Count4/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number:Int = 0
+    
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus() {
+        number = number - 1
+        label.text = String(number)
+    }
+
+    @IBAction func clear() {
+        number = 0
+        label.text = String(number)
+    }
 
 }
 
